b63bfabd80986eaec05ba774f1bbc12979797bee
konstantin.b.ysbm@gmail.com <admin@upmedio.com>
2018-02-14 09:57:33 +0200

    2.7.2018 konstantin

--- scripts/main.js
+++ scripts/main.js
@@ -159,3 +159,11 @@ jQuery(document).ready(function ($) {
         }
     });
 });
+
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i=0; i<vidDefer.length; i++) {
+        if(vidDefer[i].getAttribute('data-src')) {
+            vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+        } } }
+window.onload = init;
